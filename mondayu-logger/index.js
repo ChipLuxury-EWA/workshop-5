1f53091b6f6b200c035ebe7a8ccd5edcfbe78299
Tom Portugez <chip_luxury@gmail.com>
2022-05-29 00:28:33 +0300

    feat: app works

--- mondayu-logger/index.js
+++ mondayu-logger/index.js
@@ -1,7 +1,15 @@
-import { Console } from 'console';
-import { createWriteStream } from 'fs';
+import { Console } from "console";
+import fs from "fs";
+import { createWriteStream } from "fs";
+
+export const myLogger = new Console({
+    stdout: fs.createWriteStream("normalStdoutStream.txt", {'flags': 'a'}),
+    stderr: fs.createWriteStream("errStdErr.txt", {'flags': 'a'}),
+});
+
+
 
 // step 1: use this example: https://melvingeorge.me/blog/save-logs-to-files-nodejs
 // export your monday logger here
 
-// step 2: rename the package and publish 
+// step 2: rename the package and publish
--- weather-app/index.js
+++ weather-app/index.js
@@ -1,16 +1,30 @@
 import { AsyncWeather } from "@cicciosgamino/openweather-apis";
 import http from "http";
+import { myLogger } from "mondayu-logger-tom-portugez";
 // import your own logger
 
 // step 1: install lib and print temp to console
-const apiKey = "<<your api key>>";
+const apiKey = "6915845220e3fe2caa6fdc114ce6e687";
 const weatherInitializer = new AsyncWeather();
 const weatherAPI = await weatherInitializer;
+
+weatherAPI.setApiKey(apiKey);
+weatherAPI.setCityId(293397);
+
+const temp = await weatherAPI.getTemperature();
+myLogger.log(`the temp in TA is ${temp}`);
 
 // step 2: server
 // create your server here from http module
 // example in the slides or here: https://nodejs.org/en/knowledge/HTTP/servers/how-to-create-a-HTTP-server/
+const requestListener = function (req, res) {
+    myLogger.log(`starting the log`)
+    res.writeHead(200);
+    res.end(`the temp in TA is ${temp}`);
+};
 
+const server = http.createServer(requestListener);
+server.listen(8080);
 // start your server
 
 // step 3: install your logger and log temp and server start
